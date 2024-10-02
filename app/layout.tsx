c26518c97690869cb2acb25820fe9177ae3e9885
Sulthan20 <sulthanh717@gmail.com>
2024-10-02 16:59:42 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./components/Header";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +25,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="container mx-auto px-4 xl:px0">
+        <Header />
         {children}
+        </div>
       </body>
     </html>
   );
