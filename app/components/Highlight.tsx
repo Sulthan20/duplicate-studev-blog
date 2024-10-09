1b6e7ddb5d4f43fe0d8f2d4ac8b8251a1cb20524
Sulthan20 <sulthanh717@gmail.com>
2024-10-10 05:25:03 +0700

    Update Studev Blog

--- app/components/Highlight.tsx
+++ app/components/Highlight.tsx
@@ -6,7 +6,7 @@ const Highlight = () => {
     return (
     <main>
       <div className="container">
-        <div className="mx-auto h-[70px] px-[76px] flex items-center justify-between">
+        <div className="mx-auto h-[105px] px-[76px] flex items-center justify-between">
           <div className="widget-header-3">
             <div className="row align-self-center">
               <div className="col-md-3 align-self-center">
@@ -21,12 +21,13 @@ const Highlight = () => {
         </div>
       </div>
 
-      <div className="loop-grid mb-30">
+      <div className="mx-auto h-[400px] px-[76px] flex items-center justify-between">
         <div className="carausel-post-1 hover-up border-radius-10 overflow-hidden transition-normal position-relative wow fadeInUp">
           <div className="thumb-overlay img-hover-slide position-relative">
             <Image src={Sejarah} alt="Sejarah Singkat Beasiswa di Indonesia.svg" className="image-overlay" />
             <div className="overlay-text">
-              <h3>Sejarah Singkat Beasiswa di Indonesia</h3>
+              <h2>Info Beasiswa</h2>
+              <h3>Perjalanan Singkat Beasiswa di Indonesia</h3>
             </div>
           </div>
         </div>
