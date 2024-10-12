2f8b92f3bcf91d9c99ca9889c9ff41ab6d13340d
Sulthan20 <sulthanh717@gmail.com>
2024-10-12 21:29:15 +0700

    Update Studev Blog

--- app/components/Highlight.tsx
+++ app/components/Highlight.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Sejarah from "../public/Sejarah Singkat Beasiswa di Indonesia.svg";
+import Ellipse from "../public/Gray Ellipse .svg"
 
 const Highlight = () => {
     return (
@@ -25,9 +26,13 @@ const Highlight = () => {
         <div className="carausel-post-1 hover-up border-radius-10 overflow-hidden transition-normal position-relative wow fadeInUp">
           <div className="thumb-overlay img-hover-slide position-relative">
             <Image src={Sejarah} alt="Sejarah Singkat Beasiswa di Indonesia.svg" className="image-overlay" />
-            <div className="overlay-text">
-              <h2>Info Beasiswa</h2>
-              <h3>Perjalanan Singkat Beasiswa di Indonesia</h3>
+            <div className="overlay-text transform -translate-y-3">
+              <h2 className={'text-kategori ${geistSans.variable} mt-1 mb-2 pr-3m pl-3 flex items-center gap-1'}>
+                INFO BEASISWA <Image src={Ellipse} alt="Gray Ellipse .svg" className="mt-1"></Image></h2>
+              <h3 className={'text-judul ${geistSans.variable} mt-3 mb-2 pr-5m pl-3'}>Perjalanan Singkat Beasiswa di Indonesia</h3>
+              <h4 className={'text-publish ${geistSans.variable} mt-5 pr-5m pl-4'}>
+                <span className="post-on">14 August 2024 12:10</span>
+              </h4>
             </div>
           </div>
         </div>
