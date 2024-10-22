83cced63ba5e5c382575e9e1d5dcfb8d0c1ffe29
Sulthan20 <sulthanh717@gmail.com>
2024-10-23 05:54:39 +0700

    Update Studev Blog

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -36,45 +36,45 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 ml-6">
+                <div className="col-lg-4 col-md-6 ml-5">
                     <li className={'text-hilight ${geistSans.variable} box-text-PL list-none pb-2'}>PRODUK</li>
                     <div className="box-with-border box-text-PL mb-10">
-                        <hr className="w-[150px] border-t"></hr>
+                        <hr className="w-[160px] border-t"></hr>
                     </div>
                     <li className={'text-footer-PL ${geistSans.variable} box-text-PL list-none'}>
                         Bimbingan Beasiswa
-                        <hr className="w-[150px] border-t mt-2 mb-2"></hr>
+                        <hr className="w-[160px] border-t mt-2 mb-2"></hr>
                     </li>
                     <li className={'text-footer-PL ${geistSans.variable} box-text-PL list-none'}>
                         Kelas IELTS Online
-                        <hr className="w-[150px] border-t mt-2 mb-2"></hr>
+                        <hr className="w-[160px] border-t mt-2 mb-2"></hr>
                     </li>
                     <li className={'text-footer-PL ${geistSans.variable} box-text-PL list-none'}>
                         Kelas TOEFL Online
-                        <hr className="w-[150px] border-t mt-2 mb-2"></hr>
+                        <hr className="w-[160px] border-t mt-2 mb-2"></hr>
                     </li>
                     <li className={'text-footer-PL ${geistSans.variable} box-text-PL list-none'}>
                         Kelas Bahasa Inggris Online
                     </li>
                 </div>
-                <div className="col-lg-4 col-md-6 ml-6">
+                <div className="col-lg-4 col-md-6 -ml-11">
                     <li className={'text-hilight ${geistSans.variable} box-text-PL list-none pb-2'}>LAINNYA</li>
                     <div className="box-with-border box-text-PL mb-10">
-                        <hr className="w-[150px] border-t"></hr>
+                        <hr className="w-[160px] border-t"></hr>
                     </div>
                     <li className={'text-footer-PL ${geistSans.variable} box-text-PL list-none'}>
                         Kebijakan Privasi
-                        <hr className="w-[150px] border-t mt-2 mb-2"></hr>
+                        <hr className="w-[160px] border-t mt-2 mb-2"></hr>
                     </li>
                     <li className={'text-footer-PL ${geistSans.variable} box-text-PL list-none'}>
                         Kontak Kami
-                        <hr className="w-[150px] border-t mt-2 mb-2"></hr>
+                        <hr className="w-[160px] border-t mt-2 mb-2"></hr>
                     </li>
                 </div>
-                <div className="col-lg-4 col-md-6 -ml-3">
+                <div className="-ml-2">
                     <li className={'text-hilight ${geistSans.variable} box-text list-none pb-2'}>KATEGORI</li>
                     <div className="box-with-border box-text mb-10">
-                        <hr className="w-[240px] border-t"></hr>
+                        <hr className="w-[270px] border-t"></hr>
                     </div>
                     <div className="box-text">
                         <div className="flex gap-2 pb-2">
